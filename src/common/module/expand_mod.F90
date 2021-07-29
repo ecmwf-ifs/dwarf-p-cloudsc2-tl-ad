0adb40544e73aa4e3abc7f6745a9784000065839
Michael Lange <Michael.Lange@ecmwf.int>
2021-07-29 14:17:39 +0100

    Common: Make arrays in CLOUDSC_GLOBAL_STATE allocatables

--- src/common/module/expand_mod.F90
+++ src/common/module/expand_mod.F90
@@ -1,6 +1,6 @@
 module expand_mod
-  use parkind1 , only : jpim, jprb
-  use yomphyder, only : state_type
+  USE PARKIND1 , ONLY : JPIM, JPRB, JPRD
+  USE YOMPHYDER, ONLY : STATE_TYPE
 
   use cloudsc_mpi_mod, only : irank, numproc
   use file_io_mod, only: input_initialize, load_scalar, load_array
@@ -39,14 +39,14 @@ contains
   subroutine load_and_expand_i1(name, field, nlon, nproma, ngptot, nblocks, ngptotg)
     ! Load into the local memory buffer and expand to global field
     character(len=*) :: name
-    integer(kind=jpim), pointer, intent(inout) :: field(:,:)
+    integer(kind=jpim), allocatable, intent(inout) :: field(:,:)
     integer(kind=jpim), intent(in) :: nlon, nproma, ngptot, nblocks
     integer(kind=jpim), intent(in), optional :: ngptotg
-    integer(kind=jpim), allocatable :: buffer(:)
+    integer(kind=jpim), allocatable :: buffer(:), rbuf(:)
     integer(kind=jpim) :: start, end, size
 
     call get_offsets(start, end, size, nlon, 1, 1, ngptot, ngptotg)
-    allocate(field(nproma, nblocks))
+    if (.not. allocated(field))  allocate(field(nproma, nblocks))
     allocate(buffer(size))
     call load_array(name, start, end, size, nlon, buffer)
     call expand(buffer, field, size, nproma, ngptot, nblocks)
@@ -56,7 +56,7 @@ contains
   subroutine load_and_expand_l1(name, field, nlon, nproma, ngptot, nblocks, ngptotg)
     ! Load into the local memory buffer and expand to global field
     character(len=*) :: name
-    logical, pointer, intent(inout) :: field(:,:)
+    logical, allocatable, intent(inout) :: field(:,:)
     integer(kind=jpim), intent(in) :: nlon, nproma, ngptot, nblocks
     integer(kind=jpim), intent(in), optional :: ngptotg
     logical, allocatable :: buffer(:), rbuf(:)
@@ -64,7 +64,7 @@ contains
     integer(kind=4), allocatable :: tmp(:)
 
     call get_offsets(start, end, size, nlon, 1, 1, ngptot, ngptotg)
-    allocate(field(nproma, nblocks))
+    if (.not. allocated(field))  allocate(field(nproma, nblocks))
     allocate(buffer(size))
     call load_array(name, start, end, size, nlon, buffer)
     call expand(buffer, field, size, nproma, ngptot, nblocks)
@@ -74,14 +74,14 @@ contains
   subroutine load_and_expand_r1(name, field, nlon, nproma, ngptot, nblocks, ngptotg)
     ! Load into the local memory buffer and expand to global field
     character(len=*) :: name
-    real(kind=JPRB), pointer, intent(inout) :: field(:,:)
+    real(kind=JPRB), allocatable, intent(inout) :: field(:,:)
     integer(kind=jpim), intent(in) :: nlon, nproma, ngptot, nblocks
     integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:)
+    real(kind=jprd), allocatable :: buffer(:), rbuf(:)
     integer(kind=jpim) :: start, end, size
 
     call get_offsets(start, end, size, nlon, 1, 1, ngptot, ngptotg)
-    allocate(field(nproma, nblocks))
+    if (.not. allocated(field))  allocate(field(nproma, nblocks))
     allocate(buffer(size))
     call load_array(name, start, end, size, nlon, buffer)
     call expand(buffer, field, size, nproma, ngptot, nblocks)
@@ -91,14 +91,14 @@ contains
   subroutine load_and_expand_r2(name, field, nlon, nlev, nproma, ngptot, nblocks, ngptotg)
     ! Load into the local memory buffer and expand to global field
     character(len=*) :: name
-    real(kind=JPRB), pointer, intent(inout) :: field(:,:,:)
+    real(kind=JPRB), allocatable, intent(inout) :: field(:,:,:)
     integer(kind=jpim), intent(in) :: nlon, nlev, nproma, ngptot, nblocks
     integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:,:)
+    real(kind=jprd), allocatable :: buffer(:,:), rbuf(:,:)
     integer(kind=jpim) :: start, end, size
 
     call get_offsets(start, end, size, nlon, 1, nlev, ngptot, ngptotg)
-    allocate(field(nproma, nlev, nblocks))
+    if (.not. allocated(field))  allocate(field(nproma, nlev, nblocks))
     allocate(buffer(size, nlev))
     call load_array(name, start, end, size, nlon, nlev, buffer)
     call expand(buffer, field, size, nproma, nlev, ngptot, nblocks)
@@ -108,14 +108,14 @@ contains
   subroutine load_and_expand_r3(name, field, nlon, nlev, ndim, nproma, ngptot, nblocks, ngptotg)
     ! Load into the local memory buffer and expand to global field
     character(len=*) :: name
-    real(kind=JPRB), pointer, intent(inout) :: field(:,:,:,:)
+    real(kind=JPRB), allocatable, intent(inout) :: field(:,:,:,:)
     integer(kind=jpim), intent(in) :: nlon, nlev, ndim, nproma, ngptot, nblocks
     integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:,:,:)
+    real(kind=jprd), allocatable :: buffer(:,:,:), rbuf(:,:,:)
     integer(kind=jpim) :: start, end, size
 
     call get_offsets(start, end, size, nlon, ndim, nlev, ngptot, ngptotg)
-    allocate(field(nproma, nlev, ndim, nblocks))
+    if (.not. allocated(field))  allocate(field(nproma, nlev, ndim, nblocks))
     allocate(buffer(size, nlev, ndim))
     call load_array(name, start, end, size, nlon, nlev, ndim, buffer)
     call expand(buffer, field, size, nproma, nlev, ndim, ngptot, nblocks)
@@ -125,40 +125,37 @@ contains
   subroutine load_and_expand_state(name, state, field, nlon, nlev, ndim, nproma, ngptot, nblocks, ngptotg)
     ! Load into the local memory buffer and expand to global field
     character(len=*) :: name
-    type(state_type), pointer, intent(inout) :: state(:)
-    real(kind=JPRB), pointer, intent(inout) :: field(:,:,:,:)
+    type(state_type), allocatable, intent(inout) :: state(:)
+    real(kind=JPRB), allocatable, target, intent(inout) :: field(:,:,:,:)
     integer(kind=jpim), intent(in) :: nlon, nlev, ndim, nproma, ngptot, nblocks
     integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:,:,:)
+    real(kind=jprd), allocatable :: buffer(:,:,:), rbuf(:,:,:)
     integer(kind=jpim) :: start, end, size
 
     integer :: b
 
     call get_offsets(start, end, size, nlon, ndim, nlev, ngptot, ngptotg)
-    allocate(state(nblocks))
-    allocate(field(nproma, nlev, 6+ndim, nblocks))
-    allocate(buffer(size, nlev, 6+ndim))
+    if (.not. allocated(state))  allocate(state(nblocks))
+    if (.not. allocated(field))  allocate(field(nproma, nlev, 3+ndim, nblocks))
+    allocate(buffer(size, nlev, 3+ndim))
 
-    call load_array(name//'_T', start, end, size, nlon, nlev, buffer(:,:,3))
-    call load_array(name//'_A', start, end, size, nlon, nlev, buffer(:,:,5))
-    call load_array(name//'_Q', start, end, size, nlon, nlev, buffer(:,:,6))
-    call load_array(name//'_CLD', start, end, size, nlon, nlev, ndim, buffer(:,:,7:))
+    call load_array(name//'_T', start, end, size, nlon, nlev, buffer(:,:,1))
+    call load_array(name//'_A', start, end, size, nlon, nlev, buffer(:,:,2))
+    call load_array(name//'_Q', start, end, size, nlon, nlev, buffer(:,:,3))
+    call load_array(name//'_CLD', start, end, size, nlon, nlev, ndim, buffer(:,:,4:))
 
+    call expand(buffer(:,:,1), field(:,:,1,:), size, nproma, nlev, ngptot, nblocks)
+    call expand(buffer(:,:,2), field(:,:,2,:), size, nproma, nlev, ngptot, nblocks)
     call expand(buffer(:,:,3), field(:,:,3,:), size, nproma, nlev, ngptot, nblocks)
-    call expand(buffer(:,:,5), field(:,:,5,:), size, nproma, nlev, ngptot, nblocks)
-    call expand(buffer(:,:,6), field(:,:,6,:), size, nproma, nlev, ngptot, nblocks)
-    call expand(buffer(:,:,7:), field(:,:,7:,:), size, nproma, nlev, ndim, ngptot, nblocks)
+    call expand(buffer(:,:,4:), field(:,:,4:,:), size, nproma, nlev, ndim, ngptot, nblocks)
     deallocate(buffer)
 
 !$OMP PARALLEL DO DEFAULT(SHARED), PRIVATE(B) schedule(runtime)
     do b=1, nblocks
-       ! state(b)%u => field(:,:,1,b)
-       ! state(b)%v => field(:,:,2,b)
-       state(b)%t => field(:,:,3,b)
-       ! state(b)%o3 => field(:,:,4,b)
-       state(b)%a => field(:,:,5,b)
-       state(b)%q => field(:,:,6,b)
-       state(b)%cld => field(:,:,7:6+ndim,b)
+       state(b)%t => field(:,:,1,b)
+       state(b)%a => field(:,:,2,b)
+       state(b)%q => field(:,:,3,b)
+       state(b)%cld => field(:,:,4:3+ndim,b)
     end do
 !$OMP end parallel do
 
@@ -229,8 +226,8 @@ contains
   end subroutine expand_i1
 
   subroutine expand_r1(buffer, field, nlon, nproma, ngptot, nblocks)
-    real(kind=jprb), intent(inout) :: buffer(nlon)
-    real(kind=jprb), intent(inout) :: field(nproma, nblocks)
+    real(kind=JPRD), intent(inout) :: buffer(nlon)
+    real(kind=JPRB), intent(inout) :: field(nproma, nblocks)
     integer(kind=jpim), intent(in) :: nlon, nproma, ngptot, nblocks
     integer :: b, gidx, bsize, fidx, fend, bidx, bend
 
@@ -263,8 +260,8 @@ contains
 
   subroutine expand_r2(buffer, field, nlon, nproma, nlev, ngptot, nblocks)
           use omp_lib
-    real(kind=jprb), intent(inout) :: buffer(nlon, nlev)
-    real(kind=jprb), intent(inout) :: field(nproma, nlev, nblocks)
+    real(kind=JPRD), intent(inout) :: buffer(nlon, nlev)
+    real(kind=JPRB), intent(inout) :: field(nproma, nlev, nblocks)
     integer(kind=jpim), intent(in) :: nlon, nlev, nproma, ngptot, nblocks
     integer :: b, gidx, bsize, fidx, fend, bidx, bend
 
@@ -296,8 +293,8 @@ contains
   end subroutine expand_r2
 
   subroutine expand_r3(buffer, field, nlon, nproma, nlev, ndim, ngptot, nblocks)
-    real(kind=jprb), intent(inout) :: buffer(nlon, nlev, ndim)
-    real(kind=jprb), intent(inout) :: field(nproma, nlev, ndim, nblocks)
+    real(kind=JPRD), intent(inout) :: buffer(nlon, nlev, ndim)
+    real(kind=JPRB), intent(inout) :: field(nproma, nlev, ndim, nblocks)
     integer(kind=jpim), intent(in) :: nlon, nlev, ndim, nproma, ngptot, nblocks
     integer :: b, gidx, bsize, fidx, fend, bidx, bend
 
