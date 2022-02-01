f40b6874c4cb333d284c99b8b7660f19c79bb314
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-02-01 12:02:04 +0100

    Make log prettier.

--- src/cloudsc2py/drivers/utils.py
+++ src/cloudsc2py/drivers/utils.py
@@ -8,22 +8,25 @@ if TYPE_CHECKING:
 def log_performance(
     nruns: int,
     timer: Type["Timer"],
-    exec_info: Dict[str, Union[bool, float]],
+    exec_info: Dict[str, Dict[str, Union[bool, float]]],
     stencil_names: Sequence[str],
 ) -> None:
-    total_time = timer.get_time("run", units="ms")
-    cpp_time = 0.0
-    call_time = 0.0
-    for name in stencil_names:
-        for key in exec_info:
-            if key.startswith(name):
-                cpp_time += exec_info[key]["total_run_cpp_time"] * 1e3
-                call_time += exec_info[key]["total_call_time"] * 1e3
-                break
+    if nruns > 0:
+        total_time = timer.get_time("run", units="ms")
+        cpp_time = 0.0
+        call_time = 0.0
+        for name in stencil_names:
+            for key in exec_info:
+                if key.startswith(name):
+                    cpp_time += exec_info[key].get("total_run_cpp_time", 0.0) * 1e3
+                    call_time += exec_info[key]["total_call_time"] * 1e3
+                    break
 
-    print(
-        f"Average run time ({nruns} runs): {total_time / nruns:.3f} ms\n"
-        f"  - GT4Py (stencil calculations): {cpp_time / nruns:.3f} ms\n"
-        f"  - GT4Py (bindings overhead): {(call_time - cpp_time) / nruns:.3f} ms\n"
-        f"  - Framework: {(total_time - call_time) / nruns:.3f} ms"
-    )
+        print(
+            f"\nAverage run time ({nruns} runs): {total_time / nruns:.3f} ms\n"
+            f"  - GT4Py (stencil calculations):"
+            f" {(cpp_time if cpp_time > 0 else call_time) / nruns:.3f} ms\n"
+            f"  - GT4Py (bindings overhead):"
+            f" {(call_time - cpp_time) / nruns:.3f} ms\n"
+            f"  - Framework: {(total_time - call_time) / nruns:.3f} ms\n"
+        )
