e41e193c71aa06c7fd3d4a9af1f1e4c73c873ef0
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-07-12 09:24:55 +0200

    Simplify grid.

--- src/cloudsc2py/cloudsc2py/framework/grid.py
+++ src/cloudsc2py/cloudsc2py/framework/grid.py
@@ -1,94 +1,79 @@
 # -*- coding: utf-8 -*-
-import abc
+from __future__ import annotations
+from functools import cached_property
 import numpy as np
-from typing import Dict
 
-from cloudsc2py.utils.typingx import Range
+
+class DimSymbol:
+    """Symbol identifying a dimension, e.g. I or I-1/2."""
+
+    _instances: dict[int, DimSymbol] = {}
+
+    name: str
+    offset: float
+
+    def __new__(cls, name: str, offset: float) -> DimSymbol:
+        key = hash((name, offset))
+        if key not in cls._instances:
+            cls._instances[key] = super().__new__(cls)
+        return cls._instances[key]
+
+    def __init__(self, name: str, offset: float) -> None:
+        self.name = name
+        self.offset = offset
+
+    def __add__(self, other: float) -> DimSymbol:
+        return DimSymbol(self.name, self.offset + other)
+
+    def __sub__(self, other: float) -> DimSymbol:
+        return self + (-other)
+
+    def __repr__(self) -> str:
+        if self.offset > 0:
+            return f"{self.name} + {self.offset}"
+        elif self.offset < 0:
+            return f"{self.name} - {-self.offset}"
+        else:
+            return f"{self.name}"
+
+
+I = DimSymbol("I", 0)
+J = DimSymbol("J", 0)
+K = DimSymbol("K", 0)
 
 
 class Grid:
-    def __init__(
-        self,
-        nx: int,
-        ny: int,
-        nz: int,
-        dims_x: str = "x",
-        dims_y: str = "y",
-        dims_z: str = "z",
-    ) -> None:
-        # number of grid points
-        assert nx >= 1 and ny >= 1 and nz >= 1
-        self.nx = nx
-        self.ny = ny
-        self.nz = nz
+    """Grid of points."""
 
-        # dimensions
-        self.dims_x = dims_x
-        self.dims_xu = dims_x + "_u"
-        self.dims_y = dims_y
-        self.dims_yv = dims_y + "_v"
-        self.dims_z = dims_z
-        self.dims_zh = dims_z + "_h"
+    def __init__(self, shape: tuple[int, ...], dims: tuple[str, ...]) -> None:
+        assert len(shape) == len(dims)
+        self.shape = shape
+        self.dims = dims
 
-    @property
-    @abc.abstractmethod
-    def dims_to_axes(self) -> Dict[str, int]:
-        pass
-
-    @property
-    @abc.abstractmethod
-    def dims_to_coords(self) -> Dict[str, Range]:
-        pass
-
-    @property
-    @abc.abstractmethod
-    def dims_to_shape(self) -> Dict[str, int]:
-        pass
+    @cached_property
+    def coords(self) -> tuple[np.ndarray, ...]:
+        return tuple(np.arange(size) for size in self.shape)
 
 
-class VerticalSliceGrid(Grid):
-    def __init__(
-        self, nx: int, nz: int, dims_x: str = "x", dims_z: str = "z"
-    ) -> None:
-        super().__init__(nx, 1, nz, dims_x, "y", dims_z)
+class ComputationalGrid:
+    """A three-dimensional computational grid consisting of mass and staggered grid points."""
 
-        # coordinates
-        self.x = np.arange(1, self.nx + 1)
-        self.xu = np.arange(0.5, self.nx + 1)
-        self.y = [1]
-        self.yv = [0.5, 1.5]
-        self.z = np.arange(1, self.nz + 2)
-        self.zh = np.arange(0.5, self.nz + 1)
+    grids: dict[tuple[DimSymbol, ...], Grid]
 
-    @property
-    def dims_to_axes(self) -> Dict[str, int]:
-        return {
-            self.dims_x: 0,
-            self.dims_xu: 0,
-            self.dims_y: 1,
-            self.dims_yv: 1,
-            self.dims_z: 2,
-            self.dims_zh: 2,
+    def __init__(self, nx: int, ny: int, nz: int) -> None:
+        self.grids = {
+            (I, J, K): Grid((nx, ny, nz), ("x", "y", "z")),
+            (I, J, K - 1 / 2): Grid((nx, ny, nz + 1), ("x", "y", "z_h")),
         }
 
-    @property
-    def dims_to_coords(self) -> Dict[str, Range]:
-        return {
-            self.dims_x: self.x,
-            self.dims_xu: self.xu,
-            self.dims_y: self.y,
-            self.dims_yv: self.yv,
-            self.dims_z: self.z,
-            self.dims_zh: self.zh,
-        }
 
-    @property
-    def dims_to_shape(self) -> Dict[str, int]:
-        return {
-            self.dims_x: self.nx,
-            self.dims_xu: self.nx + 1,
-            self.dims_y: self.ny,
-            self.dims_yv: self.ny + 1,
-            self.dims_z: self.nz + 1,
-            self.dims_zh: self.nz + 1,
-        }
+def get_mask(grid_id: tuple[DimSymbol, ...], data_shape: tuple[int, ...]) -> list[bool]:
+    """Compute the mask for a storage defined over `grid_id`."""
+    out = []
+    for target_name in ("I", "J", "K"):
+        for dim_symbol in grid_id:
+            if dim_symbol.name == target_name:
+                out.append(True)
+                break
+        out.append(False)
+    return out + [True] * len(data_shape)
