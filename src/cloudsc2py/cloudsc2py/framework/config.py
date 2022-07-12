ea6398f9dd717f7a0b87f08c35cc4f2d2c9f8ea4
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-07-12 11:05:09 +0200

    Introduce config.

---
+++ src/cloudsc2py/cloudsc2py/framework/config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from pydantic import BaseModel
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any, Optional, Union
+
+
+class DataTypes(BaseModel):
+    bool: type
+    float: type
+    int: type
+
+
+class GT4PyConfig(BaseModel):
+    backend: str
+    backend_opts: dict[str, Any] = {}
+    build_info: Optional[dict[str, Any]] = None
+    device_sync: bool = True
+    dtypes: DataTypes = DataTypes(bool=bool, float=float, int=int)
+    exec_info: Optional[dict[str, Any]] = None
+    managed: Union[bool, str] = "gt4py"
+    rebuild: bool = False
+    validate_args: bool = False
+    verbose: bool = True
