c906bdef97ff6d54114bd6163a20f3eaac23efcc
Stefano Ubbiali <subbiali@phys.ethz.ch>
2022-07-13 09:43:10 +0200

    Adapt saturation.

--- src/cloudsc2py/cloudsc2py/physics/common/saturation.py
+++ src/cloudsc2py/cloudsc2py/physics/common/saturation.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+from __future__ import annotations
+from functools import cached_property
 from typing import Optional, TYPE_CHECKING
 
 from cloudsc2py.framework.components import DiagnosticComponent
+from cloudsc2py.framework.grid import I, J, K
 
 if TYPE_CHECKING:
     from sympl._core.typingx import PropertyDict
 
-    from cloudsc2py.framework.grid import Grid
-    from cloudsc2py.framework.options import BackendOptions, StorageOptions
-    from cloudsc2py.utils.typingx import ArrayDict, ParameterDict
+    from cloudsc2py.framework.config import GT4PyConfig
+    from cloudsc2py.framework.grid import ComputationalGrid
+    from cloudsc2py.utils.typingx import ParameterDict, StorageDict
 
 
 class Saturation(DiagnosticComponent):
@@ -16,56 +19,40 @@ class Saturation(DiagnosticComponent):
 
     def __init__(
         self,
-        grid: "Grid",
+        computational_grid: ComputationalGrid,
         kflag: int,
         lphylin: bool,
-        yoethf_parameters: Optional["ParameterDict"] = None,
-        yomcst_parameters: Optional["ParameterDict"] = None,
+        yoethf_parameters: Optional[ParameterDict] = None,
+        yomcst_parameters: Optional[ParameterDict] = None,
         *,
         enable_checks: bool = True,
-        backend: str = "numpy",
-        backend_options: Optional["BackendOptions"] = None,
-        storage_options: Optional["StorageOptions"] = None,
+        gt4py_config: GT4PyConfig,
     ) -> None:
-        super().__init__(
-            grid,
-            enable_checks=enable_checks,
-            backend=backend,
-            backend_options=backend_options,
-            storage_options=storage_options,
-        )
+        super().__init__(computational_grid, enable_checks=enable_checks, gt4py_config=gt4py_config)
 
         externals = {"KFLAG": kflag, "LPHYLIN": lphylin, "QMAX": 0.5}
         externals.update(yoethf_parameters or {})
         externals.update(yomcst_parameters or {})
-        self.bo.externals.update(externals)
-        self.saturation = self.compile_stencil("saturation")
+        self.saturation = self.compile_stencil("saturation", externals)
 
-    @property
-    def input_properties(self) -> "PropertyDict":
-        g = self.grid
+    @cached_property
+    def _input_properties(self) -> PropertyDict:
         return {
-            "f_ap": {"dims": (g.dims_x, g.dims_y, g.dims_z), "units": "Pa"},
-            "f_t": {"dims": (g.dims_x, g.dims_y, g.dims_z), "units": "K"},
+            "f_ap": {"grid": (I, J, K), "units": "Pa"},
+            "f_t": {"grid": (I, J, K), "units": "K"},
         }
 
-    @property
-    def diagnostic_properties(self) -> "PropertyDict":
-        g = self.grid
-        return {
-            "f_qsat": {
-                "dims": (g.dims_x, g.dims_y, g.dims_z),
-                "units": "g g^-1",
-            }
-        }
+    @cached_property
+    def _diagnostic_properties(self) -> PropertyDict:
+        return {"f_qsat": {"grid": (I, J, K), "units": "g g^-1"}}
 
-    def array_call(self, state: "ArrayDict", out: "ArrayDict") -> None:
+    def array_call(self, state: StorageDict, out: StorageDict) -> None:
         self.saturation(
             in_ap=state["f_ap"],
             in_t=state["f_t"],
             out_qsat=out["f_qsat"],
             origin=(0, 0, 0),
-            domain=(self.grid.nx, self.grid.ny, self.grid.nz),
-            validate_args=self.bo.validate_args,
-            exec_info=self.bo.exec_info,
+            domain=self.computational_grid.grids[I, J, K].shape,
+            validate_args=self.gt4py_config.validate_args,
+            exec_info=self.gt4py_config.exec_info,
         )
--- src/cloudsc2py/cloudsc2py/physics/common/stencils/saturation.py
+++ src/cloudsc2py/cloudsc2py/physics/common/stencils/saturation.py
@@ -2,50 +2,28 @@
 from gt4py import gtscript
 
 from cloudsc2py.framework.stencil import stencil_collection
+from cloudsc2py.physics.common.stencils.fcttre import foealfa, foeewm, foeewmcu
 from cloudsc2py.utils.f2py import ported_function
 
 
 @ported_function(from_file="clouds2_nl/satur.F90", from_line=106, to_line=140)
-@stencil_collection(
-    "saturation",
-    external_names=[
-        "KFLAG",
-        "LPHYLIN",
-        "QMAX",
-        "R2ES",
-        "R3IES",
-        "R3LES",
-        "R4IES",
-        "R4LES",
-        "RETV",
-        "RTT",
-        "foealfa",
-        "foeewm",
-        "foeewmcu",
-    ],
-)
+@stencil_collection("saturation")
 def saturation_def(
-    in_ap: gtscript.Field["ftype"],
-    in_t: gtscript.Field["ftype"],
-    out_qsat: gtscript.Field["ftype"],
+    in_ap: gtscript.Field["float"], in_t: gtscript.Field["float"], out_qsat: gtscript.Field["float"]
 ):
-    from __externals__ import ext
+    from __externals__ import KFLAG, LPHYLIN, QMAX, R2ES, R3IES, R3LES, R4IES, R4LES, RETV, RTT
 
     with computation(PARALLEL), interval(...):
-        if __INLINED(ext.LPHYLIN):
-            alfa = ext.foealfa(in_t)
-            foeewl = ext.R2ES * exp(
-                ext.R3LES * (in_t - ext.RTT) / (in_t - ext.R4LES)
-            )
-            foeewi = ext.R2ES * exp(
-                ext.R3IES * (in_t - ext.RTT) / (in_t - ext.R4IES)
-            )
+        if __INLINED(LPHYLIN):
+            alfa = foealfa(in_t)
+            foeewl = R2ES * exp(R3LES * (in_t - RTT) / (in_t - R4LES))
+            foeewi = R2ES * exp(R3IES * (in_t - RTT) / (in_t - R4IES))
             foeew = alfa * foeewl + (1 - alfa) * foeewi
-            qs = min(foeew / in_ap, ext.QMAX)
+            qs = min(foeew / in_ap, QMAX)
         else:
-            if __INLINED(ext.KFLAG == 1):
-                ew = ext.foeewmcu(in_t)
+            if __INLINED(KFLAG == 1):
+                ew = foeewmcu(in_t)
             else:
-                ew = ext.foeewm(in_t)
-            qs = min(ew / in_ap, ext.QMAX)
-        out_qsat = qs / (1.0 - ext.RETV * qs)
+                ew = foeewm(in_t)
+            qs = min(ew / in_ap, QMAX)
+        out_qsat = qs / (1.0 - RETV * qs)
