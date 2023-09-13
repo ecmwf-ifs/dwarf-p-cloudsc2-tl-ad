f4e328d5b481e4e1be66646dbbd5dfd9da77c076
stubbiali <subbiali@phys.ethz.ch>
2023-09-13 09:37:40 +0200

    Revisit packaging.

--- src/cloudsc2_gt4py/prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p /Users/subbiali/Desktop/kilos/cloudsc2/gt4py/src/cloudsc2py/gt_cache
-export GT_CACHE_ROOT=/Users/subbiali/Desktop/kilos/cloudsc2/gt4py/src/cloudsc2py/gt_cache
-export GT_CACHE_DIR_NAME=.gt_cache
