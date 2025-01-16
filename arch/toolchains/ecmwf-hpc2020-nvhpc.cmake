f7d8a212d4d2254ef9a7d1ed9976cc23e5d72216
Michael Lange <Michael.Lange@ecmwf.int>
2025-01-16 13:55:39 +0000

    Arch: Update NVHPC toolchain to the version from CLOUDSC

--- arch/toolchains/ecmwf-hpc2020-nvhpc.cmake
+++ arch/toolchains/ecmwf-hpc2020-nvhpc.cmake
@@ -11,29 +11,41 @@
 ####################################################################
 
 set( ECBUILD_FIND_MPI ON )
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-
-set(CMAKE_C_COMPILER nvc)
-set(CMAKE_CXX_COMPILER nvc++)
-set(CMAKE_Fortran_COMPILER nvfortran)
 
 ####################################################################
 # OpenMP FLAGS
 ####################################################################
 
-# Note that we do not support device offload via OpenMP (yet!)
-set( OpenMP_C_FLAGS             "-mp -mp=bind,allcores,numa" )
-set( OpenMP_CXX_FLAGS           "-mp -mp=bind,allcores,numa" )
-set( OpenMP_Fortran_FLAGS       "-mp -mp=bind,allcores,numa" )
+# Note: OpenMP_Fortran_FLAGS gets overwritten by the FindOpenMP module
+# unless its stored as a cache variable
+set( OpenMP_Fortran_FLAGS   "-mp -mp=gpu,bind,allcores,numa" CACHE STRING "" )
+
+# Note: OpenMP_C_FLAGS and OpenMP_C_LIB_NAMES have to be provided _both_ to
+# keep FindOpenMP from overwriting the FLAGS variable (the cache entry alone
+# doesn't have any effect here as the module uses FORCE to overwrite the
+# existing value)
+set( OpenMP_C_FLAGS         "-mp -mp=bind,allcores,numa" CACHE STRING "" )
+set( OpenMP_C_LIB_NAMES     "acchost" CACHE STRING "")
 
 ####################################################################
 # OpenAcc FLAGS
 ####################################################################
 
-set( OpenACC_Fortran_FLAGS "-acc=gpu -gpu=lineinfo,fastmath,rdc" CACHE STRING "" )
-
+# Importantly, enable `gvmode` to remove the limit of 32 vector threads
+# per thread block
+# NB: We have to add `-mp` again to avoid undefined symbols during linking
+# (smells like an Nvidia bug)
+set( OpenACC_Fortran_FLAGS "-acc=gpu -mp=gpu -gpu=cc80,lineinfo,fastmath,gvmode" CACHE STRING "" )
 # Enable this to get more detailed compiler output
 # set( OpenACC_Fortran_FLAGS "${OpenACC_Fortran_FLAGS} -Minfo" )
+
+####################################################################
+# CUDA FLAGS
+####################################################################
+
+if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
+  set(CMAKE_CUDA_ARCHITECTURES 80)
+endif()
 
 ####################################################################
 # COMMON FLAGS
